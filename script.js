fdab0f7ca779f728d418287525a305b5a8c27e7d
Paola Oliveira <80711521+paola2oliveira@users.noreply.github.com>
2021-03-27 00:56:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("enviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" &&
+     document.getElementById("email").value != "" && 
+     document.getElementById("telefone").value != "") {
+        alert("Obrigada por se cadastrar! Você recebera as novidades por e-mail")
+  }else {
+      alert("Preencha todos os campos")
+  } 
+}
